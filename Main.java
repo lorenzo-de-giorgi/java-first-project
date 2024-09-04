476c7a1f9afaff7b8d22a5612b1a9dca133399c0
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-04 16:13:22 +0200

    creata la prima condizione if in Java per controlalre se un numero è paro o disparo

--- Main.java
+++ Main.java
@@ -3,5 +3,17 @@ public class Main {
         
         // stampo un Hello Word!
         System.out.println("Hello Word!");
+
+        // creo una funzione per verificare che il numero sia pari
+
+        // definisco num come inter che in qunesto caso è 10
+        int num = 10;
+
+        // controllo che il resto della divisione per 2 sia uguale a 0 in caso positivo il numero è pari
+        if (num % 2 == 0) {
+            System.out.println("Il numero: " + num + " è paro!");
+        } else {
+            System.out.println("Il numero: " + num + " è disparo!");
+        }
     }
 }
