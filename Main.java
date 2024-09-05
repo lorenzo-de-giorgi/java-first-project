0c79f34f8d4d0bab3ae1abce5790903dcb700662
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-05 17:41:04 +0200

    creato piccolo programma che prende in inputi il nome e dopo manda un saluto in console

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] a){
+        Scanner scanner = new Scanner(System.in);
 
+        // stampo in console la richeista del nome
+        System.out.println("Inserisci il tuo nome: ");
+        // in questo modo scannerizzo la riga sotto il println
+        String nome = scanner.nextLine();
+
+        // stampo un saluto al nome inserito prima
+        System.out.println("Ciao " + nome);
     }
 }
