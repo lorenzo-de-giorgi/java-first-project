3c1fa37419771935e374ab68c8b101d4f02ed4af
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-17 16:21:02 +0200

    definisco il valore di ogni elemento

--- Main.java
+++ Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] a){
         // creo un array con quattro elementi
         int nums[] = new int[4];
+            // definisco ogni elemento
+            nums[0] = 0;
+            nums[1] = 1;
+            nums[2] = 2;
+            nums[3] = 3;
     }
 }
