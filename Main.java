55a32e39300b6c18a2dc43abdeb448f16ea65fed
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-18 16:50:55 +0200

    created class Student to create object

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
+
 public class Main {
-    public static void main(String[] a){
-        // creo un array con quattro elementi
-        int nums[] = new int[4];
-        // definisco ogni elemento
-        nums[0] = 0;
-        nums[1] = 1;
-        nums[2] = 2;
-        nums[3] = 3;
-        // ciclo for per stampare tutti gli elementi dell'array
-        for(int i=0; i<nums.length; i++){
-            System.out.println(nums[i]);
-        }
+    public static void main(String a[]){
+
     }
 }
