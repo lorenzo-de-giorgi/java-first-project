d378e80cddea46b821adb3afef5df2d50a500f82
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-19 16:54:22 +0200

    created data type in private

--- Main.java
+++ Main.java
@@ -1,27 +1,11 @@
-class Student{
-    int rollno;
-    String name;
-    int marks;
+// accedere a dati di tipo private
+class Human{
+    private int age;
+    private String name;
 }
 
 public class Main {
     public static void main(String a[]){
-        Student s1 = new Student();
-        s1.rollno = 1;
-        s1.name = "Lorenzo";
-        s1.marks = 10;
 
-        Student s2 = new Student();
-        s2.rollno = 2;
-        s2.name = "Matteo";
-        s2.marks = 9;
-
-        Student students[] = new Student[2];
-        students[0] = s1;
-        students[1] = s2;
-
-        for(int i=0; i<students.length; i++){
-            System.out.println(students[i].name + "(" + students[i].rollno + ")" + " : " + students[i].marks);
-        }
     }
 }
