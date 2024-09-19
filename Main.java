a48702bfed01252e7587389a0e6cd9f6c2e05bb9
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-19 17:08:06 +0200

    complete default constructor

--- Main.java
+++ Main.java
@@ -8,11 +8,22 @@ class Human{
         age = 12;
         name = "John";
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
 }
 
 public class Main {
     public static void main(String a[]){
         // default
         Human obj = new Human();
+        System.out.println(obj.getName() + " : " + obj.getAge());
+        // parameterized
+
     }
 }
