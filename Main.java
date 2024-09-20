12c1621cb1ea29c1b29fa59bed2cfaf68644016e
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-20 17:09:10 +0200

    called the method multi and dive in main file with the print in console

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@
 
 public class Main {
     public static void main(String a[]){
-        Calc obj = new Calc();
+        // Calc obj = new Calc();
+        AdvCalc obj = new AdvCalc();
         int r1 = obj.add(4, 5);
         int r2 = obj.sub(7, 3);
+        int r3 = obj.multi(5, 3);
+        int r4 = obj.div(15, 4);
 
-        System.out.println(r1 + " " + r2);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
     }
 }
