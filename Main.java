e11ecd94c82c6cd6b23d6342d5258473257a0dac
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-20 17:06:40 +0200

    called the class in main file with the print of the results

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@
 
 public class Main {
     public static void main(String a[]){
-        
+        Calc obj = new Calc();
+        int r1 = obj.add(4, 5);
+        int r2 = obj.sub(7, 3);
+
+        System.out.println(r1 + " " + r2);
     }
 }
