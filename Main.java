fd6c62506ade2bf7d6ac02944c002b2284bea344
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-23 23:05:11 +0200

    called the function power

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         int r2 = obj.sub(7, 3);
         int r3 = obj.multi(5, 3);
         int r4 = obj.div(15, 4);
+        double r5 = obj.power(4, 2);
 
         System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
     }
