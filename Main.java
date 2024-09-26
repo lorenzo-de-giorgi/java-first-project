50bbb5fa0b5864961680b37bf98e31f6530db02d
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-26 14:24:12 +0200

    calle dthe method power of VeryAdvCalc and printed the result

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
         int r2 = obj.sub(7, 3);
         int r3 = obj.multi(5, 3);
         int r4 = obj.div(15, 4);
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        double r5 = obj.power(4, 2);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
