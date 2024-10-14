48427000341190ee1d4612cd21ed1c9b8aacdd9e
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-14 11:14:05 +0200

    created class B with A interface with two methods

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ interface A {
     void config();
 }
 
+class B implements A{
+
+    public void show(){
+        System.out.println("in show");
+    }
+
+    public void config(){
+        System.out.println("in config");
+    }
+}
+
 public class Main {
     public static void main(String a[]){
 
