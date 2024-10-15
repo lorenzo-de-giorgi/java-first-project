d01ec7a97b36877cfd8b73c3886660a84b8a78f2
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-15 12:20:28 +0200

    enum in java how to print with for

--- Main.java
+++ Main.java
@@ -1,31 +1,16 @@
-interface Computer {
-    void code();
+enum Status{
+    Running, Failed, Pending, Success;
 }
 
-class Laptop implements Computer{
-    public void code(){
-        System.out.println("code, compile, run");
-    }
-}
-
-class Desktop implements Computer{
-    public void code(){
-        System.out.println("code, compile, faster");
-    }
-}
-
-class Developer{
-    public void devApp(Computer lap){
-        lap.code();
-    }
-}
-
-public class Main{
+public class Main {
     public static void main(String[] args){
-        Computer lap = new Laptop();
-        Computer desk = new Desktop();
+        // stamapre tutti i satus insieme
+        Status[] ss = Status.values();
+        System.out.println(ss);
 
-        Developer degio = new Developer();
-        degio.devApp(desk);
+        for(Status s:ss){
+            System.out.println(s);
+            System.out.println(s+ " : "+s.ordinal());
+        }
     }
 }
