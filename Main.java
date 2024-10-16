72359b085b69badeced05a74bb99dc2fe83280fb
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-16 17:47:57 +0200

    lambda expression

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
 // accetta un solo metodo s ene metto due da un errore
 @FunctionalInterface
 interface A{
-    void show();
+    void show(int i);
 }
-
-// class B implements A{
-//     public void show(){
-//         System.out.println("in A show");
-//     }
-// }
 
 public class Main {
     public static void main(String a[]){
-        A obj = new A() {
-            public void show(){
-                System.out.println("in A show");
-            }
-        };
-
-        obj.show();
+        A obj = (int i) -> System.out.println("in show" + i);
+        obj.show(5);
     }
 }
