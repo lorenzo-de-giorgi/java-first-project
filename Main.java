3b2d176a2b2629e4f252fc1a6386888a85d5a94b
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-16 18:01:48 +0200

    functional interface with return

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 // accetta un solo metodo s ene metto due da un errore
 @FunctionalInterface
 interface A{
-    void show(int i);
+    int add(int i, int j);
 }
 
 public class Main {
     public static void main(String a[]){
-        A obj = (int i) -> System.out.println("in show" + i);
-        obj.show(5);
+        A obj = (i, j) -> i+j;
+
+        int result = obj.add(5, 4);
+        System.out.println(result);
     }
 }
