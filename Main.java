e0b985d2ef8e84d3090633de18a35e8aff3e656c
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-16 17:30:27 +0200

    annotations in java

--- Main.java
+++ Main.java
@@ -1,35 +1,20 @@
-enum Laptop{
-    // MacBook(2000), XPS(2200), Surface(1500), ThinkPad(1800);
-    MacBook(2000), XPS(2200), Surface, ThinkPad(1800);
 
-    private int price;
-
-    private Laptop(){
-        price = 500;
+class A {
+    public void show(){
+        System.out.println("in A show");
     }
+}
 
-    private Laptop(int price){
-        this.price = price;
+class B extends A{
+    @Override
+    public void show(){
+        System.out.println("in B show");
     }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
 }
 
 public class Main {
-    public static void main(String[] args){
-        
-        // Laptop lap = Laptop.MacBook;
-        // System.out.println(lap + " : " + lap.getPrice());
-
-        for(Laptop lap : Laptop.values()){
-            System.out.println(lap + " : " + lap.getPrice());
-        }
+    public static void main(String a[]){
+        B obj = new B();
+        obj.show();
     }
 }
