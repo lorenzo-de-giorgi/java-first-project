1e730719260d1b665004d460275e163004cdaac4
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-17 21:30:21 +0200

    Custom Exception

--- Main.java
+++ Main.java
@@ -1,19 +1,38 @@
+class DegioException extends Exception
+{
+	public DegioException(String string)
+	{
+		super(string);
+	}
+}
 public class Main {
-    public static void main(String a[]){
-        int i = 0;
-        int j = 0;
+    public static void main(String[] args) {
 
-        try {
-            j = 18/i;
-            if(j == 0)
-                throw new ArithmeticException("I don't want to do print zero");
-        } catch (ArithmeticException e) {
-            j = 18/i;
-            System.out.println("that is default output"+e);
-        } catch(Exception e) {
+    	int i=20;
+    	int j=0;
+ 
+    	
+    	try
+    	{
+    		j=18/i;
+    		if(j==0)
+				//throw new Exception("I don't want to do print zero");
+    			throw new DegioException("I don't want to do print zero");
+    	}
+
+    	catch(ArithmeticException e)
+    	{
+    		j=18/i;
+    		System.out.println("that is default output"+e);
+
+			//System.out.println("Cannot divide by zero");
+    	}
+    	
+    	catch(Exception e)
+    	{
+  
     		System.out.println("Something went wrong."+e);
     	}
-        
     	System.out.println(j);
     	System.out.println("Bye");
     }
