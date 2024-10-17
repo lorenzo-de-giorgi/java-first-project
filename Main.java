507d58794b1dd5ee8ddde73879cd0549dc0378a4
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-17 21:20:29 +0200

    Exception with multiple catch

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void main(String a[]){
-        int i = 0;
+        int i = 2;
         int j = 0;
+
+        int nums[] = new int[5];
+        String str = null;
 
         try {
             j = 18/i;
-        } catch (Exception e) {
-            System.out.println("Something went wrong");
-        }
+            System.out.println(str.length());
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Stay in your limit.");
+        } catch(Exception e) {
+  
+    		System.out.println("Something went wrong."+e);
+    	}
 
         System.out.println(j);
-
-        System.out.println("Bye");
+    	System.out.println("Bye");
     }
 }
