7719127243e82a73319606000b05b2c0a324949e
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-18 12:31:57 +0200

    user input using bufferreader and scanner

--- Main.java
+++ Main.java
@@ -1,31 +1,14 @@
-class DegioException extends Exception
-{
-	public DegioException(String string)
-	{
-		super(string);
-	}
-}
-
-class A{
-	public void show() throws ClassNotFoundException
-	{
-		Class.forName("Calc");
-	}
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-	static {
-		System.out.println("Class Loader");
-	}
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a number:");
 
-		A obj = new A();
-
-		try {
-			obj.show();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
     }
 }
