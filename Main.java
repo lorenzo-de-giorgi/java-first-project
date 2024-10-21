0b6036c8255dcf1c78f45f9ba790a38eb15b789d
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-21 21:49:23 +0200

    Sets

--- Main.java
+++ Main.java
@@ -1,31 +1,55 @@
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.Collection;
-import java.util.List;
+import java.util.Iterator;
 
-public class Main {
+/*
+import java.util.List;
+import java.util.ArrayList;
+
+public class Demo {
     public static void main(String[] args){   
     	
-//    	Collection<Integer> nums= new ArrayList<Integer>();
-//  	Collection nums=new ArrayList();
     	List<Integer> nums=new ArrayList<Integer>();
     	nums.add(6);
     	nums.add(5);
     	nums.add(8);
     	nums.add(2);
+    	nums.add(6);
     	//nums.add("5");
     	
-    	System.out.println(nums.get(2));
-    	
-    	System.out.println(nums.indexOf(2));
-    	
-//    	for(int n:nums)
-//    	{
-//    		System.out.println(nums);	
-//    	}
     	for(Object n:nums)
     	{
     		int num=(Integer)n;
-    		System.out.println(nums);	
+    		System.out.println(num+2);
     	}
+    }
+}
+*/
+
+
+public class Main {
+    public static void main(String[] args){   
+    	
+//    	Set<Integer> nums=new HashSet<Integer>();  	
+//    	Set<Integer> nums=new TreeSet<Integer>();
+    	Collection<Integer> nums=new TreeSet<Integer>();
+    	nums.add(62);
+    	nums.add(54);
+    	nums.add(82);
+    	nums.add(21);
+    	
+    	//nums.add("5");
+    	
+    	Iterator<Integer> values = nums.iterator();
+    	
+    	while(values.hasNext())
+    		System.out.println(values.next());
+    	
+//    	for(int n:nums)
+//    	{
+//    		System.out.println(n);
+//   	}
     }
 }
