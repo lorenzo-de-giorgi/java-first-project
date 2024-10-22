5e1f3616748aa3ad7cf51cbfe7129277fd5a983c
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-22 21:40:14 +0200

    Need Of Stream API

--- Main.java
+++ Main.java
@@ -1,83 +1,38 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Comparator;
-
-
-//class Student implements Comparable<Student>
-class Student
-{
-	int age;
-	String name;
-	
-	public Student(int age, String name)
-	{
-		this.age=age;
-		this.name=name;
-	}
-	
-	public String toString() {
-		return "Student [age=" + age + ", name=" +name +"]";
-	}
-	
-//	public int CompareTo(Student that)
-//	{
-////		return 0;
-//		if(this.age >that.age)
-//			return 1;
-//		else
-//			return -1;
-//	}
-}
-	
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args){   
     	
-//    	Comparator<Integer> com=new Comparator<Integer>()
+    	List<Integer> nums= Arrays.asList(4,5,7,3,2,6);
+    	
+//    	for(int i=0;i<nums.size();i++)
 //    	{
-//    		public int compare(Integer i,Integer j)
-//    		{
-//    			if(i%10 >j%10)
-//    				return 1;
-//    			else
-//    				return -1;
-//    		}
-//    	};	
+//    		System.out.println(nums.get(i));
+//    	}
     	
-//    	List<Integer> nums= new ArrayList<>();
-//    	nums.add(43);
-//    	nums.add(31);
-//    	nums.add(72);
-//    	nums.add(29);
     	
-//    	Comparator<Student> com=new Comparator<Student>()
+//    	for(int n: nums)
 //    	{
-//    		public int compare(Student i,Student j)
-//    		{
-//    			if(i.age >j.age)
-//    				return 1;
-//    			else
-//    				return -1;
-//    		}
-//    	};	
+//    		System.out.println(n);
+//    	}
     	
-    	Comparator<Student> com=(i,j) -> i.age > j.age?1:-1;
-     	
-    	List<Student> studs= new ArrayList<>();
-    	studs.add(new Student(21,"Navin"));
-    	studs.add(new Student(12,"John"));
-    	studs.add(new Student(18,"Parul"));
-    	studs.add(new Student(20,"Kiran"));
+    	nums.forEach(n -> System.out.println(n));
     	
-//    	Collections.sort(nums);
+    	
+    	int sum=0;
+    	for(int n:nums)
+    	{
+    		if(n%2==0)
+    		{
+    			n=n*2;
+    			sum=sum+n;
+    		}
+    	}
+    	
 //    	System.out.println(nums);
-    	
-    	for(Student s:studs)
-    		System.out.println();
-    	
-    	Collections.sort(studs);
-    	for(Student s: studs)
-    		System.out.println(s);
+    	System.out.println(sum);
+  
     }
 }
