72f4f312c350e1a091b3cba0bf6e34d2e266620e
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-22 21:45:55 +0200

    forEach

--- Main.java
+++ Main.java
@@ -1,38 +1,30 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args){   
     	
     	List<Integer> nums= Arrays.asList(4,5,7,3,2,6);
     	
-//    	for(int i=0;i<nums.size();i++)
-//    	{
-//    		System.out.println(nums.get(i));
-//    	}
+//    	Consumer<Integer> con=new Consumer<Integer>() {
+//    		
+//    		public void accept(Integer n)
+//    		{
+//    			System.out.println(n);
+//    		}
+//    	};
     	
-    	
-//    	for(int n: nums)
-//    	{
-//    		System.out.println(n);
-//    	}
-    	
+    	Consumer<Integer> con= n -> System.out.println(n);
+ 
     	nums.forEach(n -> System.out.println(n));
+   
+ //  	nums.forEach(null);
+ //   	nums.forEach(con);
     	
     	
-    	int sum=0;
-    	for(int n:nums)
-    	{
-    		if(n%2==0)
-    		{
-    			n=n*2;
-    			sum=sum+n;
-    		}
-    	}
+    	//nums.forEach(n -> System.out.println(n));
     	
-//    	System.out.println(nums);
-    	System.out.println(sum);
-  
     }
 }
